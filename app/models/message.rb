bd0ee0919927c5de600cc883e03c432ec064814d
Fernando Guillen <fguillen.mail@gmail.com>
2024-07-06 23:05:43 +0200

    Syntax highlighting in function call arguments

--- app/models/message.rb
+++ app/models/message.rb
@@ -17,4 +17,27 @@ class Message < ApplicationRecord
 
   scope :order_by_recent, -> { order("messages.created_at desc") }
   scope :in_order, -> { order("messages.order asc") }
+
+  def self.extract_function_arguments(tool_call)
+    puts ">>>> tool_call: #{tool_call}"
+    case tool_call["function"]["name"]
+    when "database__execute"
+      JSON.parse(tool_call["function"]["arguments"])["input"]
+    when "database__describe_tables"
+      JSON.parse(tool_call["function"]["arguments"])["tables"]
+    else
+      raise "unknown tool call name: '#{tool_call["function"]["name"]}'"
+    end
+  end
+
+  def self.extract_function_arguments_language(tool_call)
+    case tool_call["function"]["name"]
+    when "database__execute"
+      "sql"
+    when "database__describe_tables"
+      "txt"
+    else
+      raise "unknown tool call name: '#{tool_call["function"]["name"]}'"
+    end
+  end
 end
