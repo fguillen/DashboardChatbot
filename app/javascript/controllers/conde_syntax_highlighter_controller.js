b4d9f12a8a47c0a9bec2b100c6d41cce23c27a38
fguillen <fguillen.mail@gmail.com>
2024-08-02 23:47:11 +0200

    Theme: Navbar

--- app/javascript/controllers/conde_syntax_highlighter_controller.js
+++ app/javascript/controllers/conde_syntax_highlighter_controller.js
@@ -1,12 +1,12 @@
 console.log(">>>> Loading conde_syntax_highlighter_controller.js");
 
 import { Controller } from "@hotwired/stimulus"
-import "prism";
+// import "prism";
 
 // Connects to data-controller="conde-syntax-highlighter"
 export default class extends Controller {
   connect() {
     console.log("CondeSyntaxHighlighter controller connected, element:", this.element)
-    Prism.highlightElement(this.element);
+    // Prism.highlightElement(this.element);
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "prism", to: "prism.js"
+# pin "prism", to: "prism.js"
 
 # chartkick
 pin "chartkick", to: "chartkick.js"
