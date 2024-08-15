0f562945a1e02d4ebed343f2405788be974bc5bf
fguillen <fguillen.mail@gmail.com>
2024-08-15 22:01:05 +0200

    Fixed bug in ConversationAssistant that was cleaning the tools responses

--- app/ai/assistants/conversation_assistant.rb
+++ app/ai/assistants/conversation_assistant.rb
@@ -1,14 +1,14 @@
 class Assistants::ConversationAssistant < AI::Assistant
-  def after_initialize
-    clean_conversation
-  end
+  # def after_initialize
+  #   clean_conversation
+  # end
 
-  def clean_conversation
-    @conversation.messages =
-      @conversation.messages.filter do |m|
-        m.role != "tool" && m.content.present?
-      end
-  end
+  # def clean_conversation
+  #   @conversation.messages =
+  #     @conversation.messages.filter do |m|
+  #       m.role != "tool" && m.content.present?
+  #     end
+  # end
 
   def system_directive
     File.read("#{Rails.root}/config/assistant_instructions_conversation_assistant.md")
