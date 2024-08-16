15788c8af05c904f03c84afd8713d3955f3d34e0
fguillen <fguillen.mail@gmail.com>
2024-08-16 23:31:55 +0200

    Alert is sent to the user that creates it

--- app/services/alerts/create_alert_email.rb
+++ app/services/alerts/create_alert_email.rb
@@ -2,7 +2,7 @@ class Alerts::CreateAlertEmail < Service
   def perform(alert:, content:)
     AlertEmail.create!(
       from: "fernando@playcocola.com",
-      to: FrontUser.order_by_recent.first.email, # TODO: Get the FrontUser from the alert
+      to: alert.front_user.email,
       subject: alert.name,
       content: content,
       alert: alert
