f85002095a3e9d1be641ea0e974e60ba5cd222f8
Fernando Guillen <fguillen.mail@gmail.com>
2024-11-29 15:03:32 +0100

    Adapting production docker compose

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,16 +24,16 @@ unless FrontUser.where(email: email).exists?
   puts "FrontUser created #{email}/#{password}"
 end
 
-tags_example = ["shopping", "cooking", "hang-out", "love", "passion"]
-10.times do
-  article =
-    Article.create!(
-      front_user: FrontUser.find_by(email: email),
-      title: Faker::Lorem.sentence,
-      body: Faker::Lorem.paragraphs(number: rand(10..20)).join("\n\n"),
-      tag_list: tags_example.sample(rand(0..3))
-    )
-  article.pic.attach(io: File.open("#{Rails.root}/test/fixtures/files/yourule.png"), filename: "yourule.png")
+# tags_example = ["shopping", "cooking", "hang-out", "love", "passion"]
+# 10.times do
+#   article =
+#     Article.create!(
+#       front_user: FrontUser.find_by(email: email),
+#       title: Faker::Lorem.sentence,
+#       body: Faker::Lorem.paragraphs(number: rand(10..20)).join("\n\n"),
+#       tag_list: tags_example.sample(rand(0..3))
+#     )
+#   article.pic.attach(io: File.open("#{Rails.root}/test/fixtures/files/yourule.png"), filename: "yourule.png")
 
-  puts "Article created: '#{article.title}'"
-end
+#   puts "Article created: '#{article.title}'"
+# end
