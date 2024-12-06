918979ca6a5f6cb9bc92cb84322aa61e4144dd7d
fguillen <fguillen.mail@gmail.com>
2024-12-06 17:07:28 +0100

    Comments

--- config/initializers/onnxruntime.rb
+++ config/initializers/onnxruntime.rb
@@ -1,3 +1,4 @@
+# I have to install the onnxruntime library by hand https://github.com/ankane/informers/issues/12
 if Rails.env.production?
   Rails.logger.info "Setting ONNX Runtime library path"
   OnnxRuntime.ffi_lib = "/usr/lib/libonnxruntime.so.1.19.2"
